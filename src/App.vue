3b78fb37650dbc0d68d3ae60dc82a3e9c69877bc
Андрей Настарчук <a.nastarchuk@denexy.local>
2019-04-15 15:07:34 +0300

    now

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App on Now 2.0"/>
+  <div>
+    <full-page ref="fullpage" :options="options" id="fullpage">
+      <div class="section">
+        First section ...
+      </div>
+      <div class="section">
+        Second section ...
+      </div>
+      <div class="section">
+        Second section ...
+      </div>
+    </full-page>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import 'animate.css'
+import 'fullpage-vue/src/fullpage.css'
 
 export default {
+  el: '#app',
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      options: {
+        menu: '#menu',
+        anchors: ['page1', 'page2', 'page3'],
+        sectionsColor: ['#41b883', '#ff5f45', '#0798ec']
+      },
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.page-container {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
 }
 </style>
